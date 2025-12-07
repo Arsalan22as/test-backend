3d0d81716a238f34904520bf88af5db6662af5e8
Arsalan22as <arsalan@example.com>
2025-12-07 17:30:22 +0530

    Initial commit

---
+++ fullstack basic/backend/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Server is ready');
+})
+
+// get a list of 5 jokes
+
+app.get('jokes', (req, res) => )
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server at http://localhost:${port}`);
+})
